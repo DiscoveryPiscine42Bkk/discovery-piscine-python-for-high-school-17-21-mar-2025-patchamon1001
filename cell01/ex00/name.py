4e4d32f62ffb9f626f3559ee54da8704f5fcea4c
patchamon1001 <nongoey1001@gmail.com.com>
2025-03-17 16:30:27 +0700

    cell1

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name="patchamon"
+last_name="ubolban"
+print(first_name+" "+last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name="patchamon" 
+last_name="ubolban"
+whole_name=first_name+" "+last_name
+print(whole_name)
